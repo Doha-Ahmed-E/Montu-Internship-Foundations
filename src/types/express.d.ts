4287dde696f17f301d8bf645bb70a0533f0f3eb7
Doha-Ahmed-E <dohaamed22@gmail.com>
2026-08-20 17:42:42 +0300

    Fix Express request type definitions

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,14 +1,10 @@
 import { JwtPayload } from "jsonwebtoken";
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: JwtPayload & {
-        userId: string;
-        role: string;
-      };
-    }
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: JwtPayload & {
+      userId: string;
+      role: string;
+    };
   }
 }
-
-export {};
